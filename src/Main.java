431d038afe12ba3f0d4aa7625c45c70a1b997daa
derossca <derossca@mail.uc.edu>
2024-02-14 15:03:02 -0500

    added variables, made do while loop, made if statement to safely read in input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        //PSEUDO CODE HERE
+        //class
+        //  main
+        //  return
+        //end class
+        //Declaring variables
+        Scanner in = new Scanner(System.in);
+        double tempC;
+        double tempF;
+        String doneYN = "";
+        String trash = "";
+        Boolean done = false;
+
+        //do while loop to execute code so long as the boolean
+        do{
+            System.out.println("Enter the temperature in Celsius:");
+            if(in.hasNextDouble())
+            {
+                tempC = in.nextDouble();
+                in.nextLine();// Clearing buffer
+                tempF = (tempC * 9/5) + 32;
+                System.out.println("The temperature in Fahrenheit is " + tempF);
+                done = true;
+            }
+            else
+            {
+                trash = in.nextLine();
+                System.out.println("You must enter a valid number not" +trash);
+            }
+        } while (!done);
     }
 }
